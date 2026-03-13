1b74726b18d89ae0fd89ed6b67babb73dc3af7ab
Matheus M <m.mdamata@hotmail.com>
2026-03-13 12:02:53 -0300

    Criando tela de login do projeto Las Esfihas

---
+++ application/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const[usuario, setUsuario] = useState(null);
+  const[email, setEmail] = useState("");
+  const[senha, setSenha] = useState("");
+
+  const[listaUsuarios] = useState([
+    {id: 1, email: "admin@admin.com", senha: "123", cargo: "admin"},
+    {id: 2, email: "cliente@cliente.com", senha: "123", cargo: "cliente"}
+  ]);
+  const realizarLogin = () =>{
+    const achado = listaUsuarios.find(u => u.email === email && u.senha === senha);
+
+    if(achado){
+      setUsuario(achado);
+    }else{
+      alert("Email ou senha incorretos!");
+    }
+  }
+
+  if(!usuario){
+    return (
+      <>
+        <header className="header"><div className="logo-text">Las Esfihas</div></header>
+        <div className="login-container">
+          <h2 className="login-title">Tela de acesso</h2>
+          <label>Email:</label>
+          <input type="email" onChange={(e) => setEmail(e.target.value)} />
+          <label>Senha:</label>
+          <input type="password" onChange={(e) => setSenha(e.target.value)} />
+          <button className="login-button" onClick={realizarLogin}>Entrar</button>
+        </div>
+      </>
+    )
+  }
+  return(
+    <>
+      <header className="header">
+        <div className="logo-text">Las Esfihas</div>
+        <button className="logout-button" onClick={() => setUsuario(null)}>Sair</button>
+      </header>
+      <main className="main-content">
+        {usuario.cargo === "admin" ? (
+            <p>Olá, administrador!</p>
+          ) : (
+            <p>Olá, cliente!</p>
+          )}
+      </main>
+    </>
+  );
+}
+
+export default App
